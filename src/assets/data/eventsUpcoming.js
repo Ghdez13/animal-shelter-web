42fe7264239d7df94c78e56ccfdc473432399fc1
Gerardo Hernandez <gerardo-hernandez03@outlook.com>
2026-01-06 21:42:43 -0500

    Add new adoption´s events

--- src/assets/data/eventsUpcoming.js
+++ src/assets/data/eventsUpcoming.js
@@ -6,42 +6,6 @@ export const eventsUpcoming = [
                 en: "Sunday",
                 fr: "Dimanche",
             },
-            dayNumber: "04",
-            month: {
-                es: "Enero",
-                en: "January",
-                fr: "Janvier",
-            },
-        },
-        location: {
-            es: "Quinta Alegria - Playa del Carmen",
-            en: "Quinta Alegria - Playa del Carmen",
-            fr: "Quinta Alegria - Playa del Carmen",
-        },
-        time: "4:00 PM - 08:00 PM",
-        start: "2026-01-04T16:00:00-05:00",
-        end: "2026-01-04T20:00:00-05:00",
-        mapUrl: "https://maps.app.goo.gl/wTxnETvNsVKJXiEG6",
-        titleHoverLocation: {
-            es: "Abrir en Google Maps",
-            en: "Open in Google Maps",
-            fr: "Ouvrir dans Google Maps",
-        },
-        titleHoverCalendar: {
-            es: "Agenda la fecha",
-            en: "Add to Calendar",
-            fr: "Ajouter à l’agenda",
-        },
-        image: "/adoptionEvents/quintaAlegria.webp",           // Desktop
-        imageMobile: "/adoptionEvents/quintaAlegriaMobile.webp" // Mobile
-    },
-    {
-        date: {
-            dayName: {
-                es: "Domingo",
-                en: "Sunday",
-                fr: "Dimanche",
-            },
             dayNumber: "11",
             month: {
                 es: "Enero",
@@ -57,6 +21,42 @@ export const eventsUpcoming = [
         time: "4:00 PM - 08:00 PM",
         start: "2026-01-11T16:00:00-05:00",
         end: "2026-01-11T20:00:00-05:00",
+        mapUrl: "https://maps.app.goo.gl/wTxnETvNsVKJXiEG6",
+        titleHoverLocation: {
+            es: "Abrir en Google Maps",
+            en: "Open in Google Maps",
+            fr: "Ouvrir dans Google Maps",
+        },
+        titleHoverCalendar: {
+            es: "Agenda la fecha",
+            en: "Add to Calendar",
+            fr: "Ajouter à l’agenda",
+        },
+        image: "/adoptionEvents/quintaAlegria.webp",           // Desktop
+        imageMobile: "/adoptionEvents/quintaAlegriaMobile.webp" // Mobile
+    },
+        {
+        date: {
+            dayName: {
+                es: "Domingo",
+                en: "Sunday",
+                fr: "Dimanche",
+            },
+            dayNumber: "18",
+            month: {
+                es: "Enero",
+                en: "January",
+                fr: "Janvier",
+            },
+        },
+        location: {
+            es: "Quinta Alegria - Playa del Carmen",
+            en: "Quinta Alegria - Playa del Carmen",
+            fr: "Quinta Alegria - Playa del Carmen",
+        },
+        time: "4:00 PM - 08:00 PM",
+        start: "2026-01-18T16:00:00-05:00",
+        end: "2026-01-18T20:00:00-05:00",
         mapUrl: "https://maps.app.goo.gl/wTxnETvNsVKJXiEG6",
         titleHoverLocation: {
             es: "Abrir en Google Maps",
